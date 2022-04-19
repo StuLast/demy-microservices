d7ab713a5570d948e1012ef0317b476f3ac1e0dc
Stu Last <stuart@spycedconcepts.uk>
2022-04-19 23:49:25 +0100

    Implement 2 of 6 k8 cluster services

--- part_1_blog/event-bus/index.js
+++ part_1_blog/event-bus/index.js
@@ -12,7 +12,7 @@ const events = [];
 app.post('/events', (req, res) => {
   const event = req.body;
   events.push(event);
-  axios.post('http://localhost:4000/events', event).catch(e => { console.log(e) });
+  axios.post('http://posts-clusterip-srv:4000/events', event).catch(e => { console.log(e) });
   axios.post('http://localhost:4001/events', event).catch(e => { console.log(e) });
   axios.post('http://localhost:4002/events', event).catch(e => { console.log(e) });
   axios.post('http://localhost:4003/events', event).catch(e => { console.log(e) });
--- part_1_blog/posts/index.js
+++ part_1_blog/posts/index.js
@@ -26,7 +26,7 @@ app.post('/posts', async (req, res) => {
     id,
     title
   }
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'PostCreated',
     data: {
       id,
@@ -47,5 +47,6 @@ app.post('/events', (req, res) => {
  * 
  */
 app.listen(4000, () => {
+  console.log('V3');
   console.log('Posts is running on  port 4000');
 })
